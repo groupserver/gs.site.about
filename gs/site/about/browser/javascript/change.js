99d554ed8f04097a06dd30c6b143ac70caaab2c6
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-01 16:37:25 +1100

    Sorting out the Change link

--- gs/site/about/browser/javascript/change.js
+++ gs/site/about/browser/javascript/change.js
@@ -1,1 +1,12 @@
 jQuery.noConflict();
+
+function gs_site_about_change_init () {
+    
+}
+
+jQuery(window).load( function () {
+    gsJsLoader.with_module(['/++resource++popup_help-20071218.js',
+                            '/++resource++wymeditor-20090831/jquery.wymeditor.js',
+                           '/++resource++gswym.js'],
+                           gs_site_about_change_init);
+});
