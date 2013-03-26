fd06e112c8bba253dad58bd56f43d69004444698
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-26 15:26:27 +1100

    Adding a minified version of the About box JS.

---
+++ gs/site/about/browser/javascript/about-min.js
@@ -0,0 +1,3 @@
+jQuery.noConflict();function gs_site_home_about_init(){GSMoreWidget("#gs-site-about-home")
+}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-more-20130110.js",gs_site_home_about_init)
+});
