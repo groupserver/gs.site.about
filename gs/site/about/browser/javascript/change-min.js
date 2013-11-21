67df5fbb0e792387d3970fe48863cfe037271a6d
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-20 17:07:25 -0800

    Dumping useless code.

--- gs/site/about/browser/javascript/change-min.js
+++
@@ -1,2 +0,0 @@
-jQuery.noConflict();function gs_site_about_change_init(){}jQuery(window).load(function(){gsJsLoader.with_module(["/++resource++popup_help-20071218.js","/++resource++wymeditor-20090831/jquery.wymeditor.js","/++resource++gswym.js"],gs_site_about_change_init)
-});
