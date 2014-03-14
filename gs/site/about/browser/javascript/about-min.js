059ce3ca4380be2f6cd45159f6b2941de2e6ef86
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-14 11:53:24 -0700

    Following the changes to gs.content.js.more, and switching on strict mode.

--- gs/site/about/browser/javascript/about-min.js
+++ gs/site/about/browser/javascript/about-min.js
@@ -1,3 +1,3 @@
 jQuery.noConflict();function gs_site_home_about_init(){GSMoreWidget("#gs-site-about-home")
-}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-more-20130110.js",gs_site_home_about_init)
+}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-more-20140313.js",gs_site_home_about_init)
 });
