6dddd047937cb8274b95570399b63a6ce2a7a6da
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-19 15:42:31 -0700

    Removing the More button from the About box.

--- gs/site/about/browser/javascript/about-min.js
+++
@@ -1,3 +0,0 @@
-jQuery.noConflict();function gs_site_home_about_init(){GSMoreWidget("#gs-site-about-home")
-}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-more-20140313.js",gs_site_home_about_init)
-});
