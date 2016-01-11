5579a7a63f4eca1f3b67c2fd6010543421599d74
Michael JasonSmith <mpj17@onlinegroups.net>
2016-01-11 09:57:01 -0800

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.site.about'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -26,7 +27,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.site.about',
+    name=name,
     version=version,
     description="The About box for a GroupServer site.",
     long_description=long_description,
@@ -46,10 +47,11 @@ setup(
     keywords='site, groupserver, introduction, edit',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.site.about/',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.site', ],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
